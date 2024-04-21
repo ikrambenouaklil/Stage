fb145719370701c4c7f3f62b6384e8ed53bc68b2
ikrambn2002@gmail.com <ikram.benouaklil@gmail.com>
2024-04-22 00:22:30 +0100

    jjj

--- backend/back.js
+++ backend/back.js
@@ -1,5 +1,18 @@
-const express = require ("express")
-const app = express()
-app.listen('3008',()=>{
-    console.log("hello hiiii"); 
-} );
+// to add the import i shuld add "type" : "module", in the package.json
+import express  from 'express'
+import cors from 'cors' 
+const app = express();
+const port = 3008
+
+app.use(cors())
+//
+app.use(express.json)
+// get informations from the database 
+app.get('/', function (req, res) {
+  res.send('hello world!');
+});
+
+
+app.listen(port, function (req, res) {
+ console.log('hello world');
+});
--- front/vite.config.js
+++ front/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],server :{port :3008 },
+  plugins: [react()]
+  // ,server :{port :3008 },
 })
